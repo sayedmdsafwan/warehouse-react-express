601bfa3affd7b6cd768f27db6cce9413956a1fcc
Md. Safwan <sayedmdsafwan@gmail.com>
2022-04-29 19:32:02 +0600

    added pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header/Header";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <h2>this is from react</h2>
+        <div>
+            <Header></Header>
+            <Routes>
+                <Route></Route>
+            </Routes>
         </div>
     );
 }
---
+++ src/components/MyItems/MyItems.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyItems = () => {
+    return <div>MyItems</div>;
+};
+
+export default MyItems;
