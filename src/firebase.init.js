35c77ccfe3dbda0b6a9e65fb88061842ef37dfe5
Md. Safwan <sayedmdsafwan@gmail.com>
2022-05-07 19:47:53 +0600

    env file added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,20 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCu6n0izqNryzCFaqvT0gLCOcy9vDrCd1s",
-    authDomain: "fruits-ninja.firebaseapp.com",
-    projectId: "fruits-ninja",
-    storageBucket: "fruits-ninja.appspot.com",
-    messagingSenderId: "925805076794",
-    appId: "1:925805076794:web:9218f03b974e72018a7003",
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.envREACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
-
-// apiKey:process.env.REACT_APP_apiKey,
-//   authDomain:process.env.REACT_APP_authDomain,
-//   projectId:process.env.REACT_APP_projectId,
-//   storageBucket:process.env.REACT_APP_storageBucket,
-//   messagingSenderId:process.env.REACT_APP_messagingSenderId,
-//   appId:process.env.REACT_APP_appId,
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
